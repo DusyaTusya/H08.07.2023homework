24d9d1f56d136c0e37b1a55e9b7484b7c7a3a5de
Dusya <Dusya@gmail.com>
2023-07-08 12:21:14 +0300

    add homework

---
+++ HomeWork9Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+// которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int ReadInt(string message) // функция чтения целого числа
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void NaturalNumbersEven(int number1, int number2) // функция по нахождению четных натуральных чисел в заданном диапазоне
+{
+    if (number1 > number2 || number1 <=0 || number2 <= 0)
+    {
+        return;
+    }
+    if (number1 % 2 == 1)
+    {
+        number1 ++;
+    }
+    System.Console.WriteLine(number1);
+    NaturalNumbersEven(number1 + 2, number2);
+}
+
+int numberM = ReadInt("Введите первое число > ");
+int numberN = ReadInt("Введите второе число > ");
+
+NaturalNumbersEven(numberM, numberN);
+
+
+
+
+
+
+
+
+
---
+++ HomeWork9Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int ReadInt(string message) // функция чтения целого числа
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNaturalNumbers(int number1, int number2) // функция по нахождению суммы натуральных чисел в заданном диапазоне
+{
+    if (number1 > number2 || number1 <=0 || number2 <= 0)
+    {
+        return 0;
+    }
+    return number1 + SumNaturalNumbers(number1+1, number2);
+}
+
+int numberM = ReadInt("Введите первое число > ");
+int numberN = ReadInt("Введите второе число > ");
+
+System.Console.WriteLine(SumNaturalNumbers(numberM, numberN));
+
